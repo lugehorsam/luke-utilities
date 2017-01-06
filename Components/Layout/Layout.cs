790225f9572d97ec24f38164cd280c0261d21821
Luke Gehorsam <lugehorsam@Lukes-Air.home>
2017-01-06 01:01:19 -0500

    more layout options

--- Components/Layout/Layout.cs
+++ Components/Layout/Layout.cs
@@ -7,12 +7,15 @@ public class Layout<TData, TBehavior> : DataManager<TData, TBehavior>
     where TBehavior : DatumBehavior<TData>
 {
 
+    [SerializeField] private Transform holder;
+    [SerializeField] private int siblingOffset;
+
     public void DoLayout (int startIndex = 0)
     {
         for (int i = startIndex; i < Behaviors.Count; i++) {
             TBehavior behavior = Behaviors [i];
-            behavior.transform.SetParent (transform, worldPositionStays: true);
-            behavior.transform.SetSiblingIndex (i);
+            behavior.transform.SetParent (holder ?? transform, worldPositionStays: false);
+            behavior.transform.SetSiblingIndex (i + siblingOffset);
             ILayoutMember layoutMember = behavior as ILayoutMember;
             if (layoutMember != null)
             {
