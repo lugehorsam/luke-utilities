de31aa18f4583d20b67c54cb39af0d2f389dd7bd
Luke Gehorsam <lugehorsam@Lukes-Air.home>
2017-06-24 22:34:18 -0400

    rect ext

---
+++ Extensions/RectExt.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class RectExt {
+
+    public static Rect Subtract(Rect rect1, Rect rect2)
+    {
+        return new Rect(rect1.x - rect2.x, rect1.y - rect2.y, rect1.width - rect2.width, rect1.height - rect2.height);
+    }
+}
