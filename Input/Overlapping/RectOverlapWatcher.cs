6031c6994c09b4fadefc721cbb992e3665883c2e
Luke Gehorsam <lugehorsam@Lukes-Air.home>
2017-08-02 23:50:26 -0400

    Working with states

--- Input/Overlapping/RectOverlapWatcher.cs
+++ Input/Overlapping/RectOverlapWatcher.cs
@@ -1,14 +1,19 @@
-﻿namespace Utilities.Input
+﻿using System.Linq;
+
+namespace Utilities.Input
 {
 	using System;
 	using System.Collections.Generic;
 	
 	public class RectOverlapWatcher
 	{
-		public event Action<IRectOverlapper, IRectOverlappee> OnOverlap = (region, snapable) => {};
-		
+		public event Action<IRectOverlapper, IRectOverlappee> OnOverlap = (overlapper, overlappee) => {};
+		public event Action<IRectOverlapper, IRectOverlappee> OnOverlapLeave = (overlapper, overlappee) => {};
+
 		public List<IRectOverlappee> Overlapees { get; } = new List<IRectOverlappee>();
 		private readonly List<IRectOverlapper> _observedOverlappers = new List<IRectOverlapper>();
+		
+		private readonly List<OverlapData> _currentOverlaps = new List<OverlapData>();
 		
 		public void Watch(IRectOverlapper rectOverlapper)
 		{
@@ -20,7 +25,7 @@
 		{
 			if (!_observedOverlappers.Contains(rectOverlapper))
 			{
-				Diagnostics.LogWarning("Trying to ignore an unwatched snapper.");
+				Diagnostics.LogWarning("Trying to unwatch an already unwatched overlapper.");
 				return;
 			}
 
@@ -28,14 +33,46 @@
 			_observedOverlappers.Remove(rectOverlapper);
 		}		
 
-		void HandleOverlapperPositionChange(IRectOverlapper changedRectOverlapper)
+		void HandleOverlapperPositionChange(IRectOverlapper overlapper)
 		{
-			foreach (IRectOverlappee region in Overlapees)
+			foreach (IRectOverlappee overlapee in Overlapees)
 			{
-				if (region.Rect.Overlaps(changedRectOverlapper.Rect))
+				bool isOverlap = overlapee.Rect.Overlaps(overlapper.Rect);
+				if (isOverlap && !IsTrackedOverlap(overlapper, overlapee))
 				{
-					OnOverlap(changedRectOverlapper, region);
+					OnOverlap(overlapper, overlapee);
+					_currentOverlaps.Add(new OverlapData(overlapper, overlapee));
 				}
+				else if (!isOverlap && IsTrackedOverlap(overlapper, overlapee))
+				{
+					OnOverlapLeave(overlapper, overlapee);
+					_currentOverlaps.Remove(GetTrackedOverlap(overlapper, overlapee));
+				}
+			}
+		}
+
+		bool IsTrackedOverlap(IRectOverlapper overlapper, IRectOverlappee overlapee)
+		{
+			return GetTrackedOverlap(overlapper, overlapee) != null;
+		}
+
+		OverlapData GetTrackedOverlap(IRectOverlapper overlapper, IRectOverlappee overlapee)
+		{
+			return _currentOverlaps.FirstOrDefault
+			(
+				overlap => overlap.RectOverlapper == overlapper && overlap.RectOverlappee == overlapee
+			);
+		}
+
+		private class OverlapData
+		{
+			public IRectOverlapper RectOverlapper { get; }
+			public IRectOverlappee RectOverlappee { get; }
+
+			public OverlapData(IRectOverlapper overlapper, IRectOverlappee overlapee)
+			{
+				RectOverlapper = overlapper;
+				RectOverlappee = overlapee;
 			}
 		}
 	}	
