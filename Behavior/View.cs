595cd8a39f6933ab400f2e0b958b7d3321e6734f
Luke Gehorsam <lugehorsam@Lukes-Air.home>
2017-09-24 00:11:02 -0400

    controller

---
+++ Behavior/View.cs
@@ -0,0 +1,24 @@
+﻿namespace Utilities
+{
+	using UnityEngine;
+	using Assets;
+	
+	[ExecuteInEditMode]
+	public abstract class Visuals<T> : MonoBehaviour where T : Bundle
+	{
+		[SerializeField] private T _bundle;
+		
+		public static T Bundle { get; set; }
+		public static bool HasBundle => Bundle != null;
+
+		protected abstract void SetVisuals(T bundle);
+
+		private void Update()
+		{
+			if (Bundle != null)
+				SetVisuals(Bundle);
+			else if (_bundle != null)
+				SetVisuals(_bundle);			
+		}
+	}
+}
