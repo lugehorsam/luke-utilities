996053879d1d7179b6bcfdc0ae1edc8dd29c90c9
Luke Gehorsam <lugehorsam@lukes-air.home>
2017-10-09 00:40:22 -0400

    more progress

---
+++ Textures/SortingLayerAssigner.cs
@@ -0,0 +1,28 @@
+﻿namespace Utilities
+{
+	using UnityEngine;
+
+	[ExecuteInEditMode]
+	public class SortingLayerAssigner : MonoBehaviour
+	{
+		[SerializeField] private string _sortingLayerName;
+		[SerializeField] private int _sortingLayerOrder;
+		
+		private void Awake()
+		{
+			AssignSortingLayer();
+		}
+		
+		private void OnValidate()
+		{
+			AssignSortingLayer();
+		}
+
+		private void AssignSortingLayer()
+		{
+			var renderer = GetComponent<Renderer>();
+			renderer.sortingLayerID = SortingLayer.NameToID(_sortingLayerName);
+			renderer.sortingOrder = _sortingLayerOrder;
+		}	
+	}
+}
